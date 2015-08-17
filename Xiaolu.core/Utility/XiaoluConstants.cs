df9b4baea00f3ef392786f8162ea579d1217e7c0
neilchennan <chen_neil@126.com>
2015-08-18 00:07:47 +0800

    test upload3

--- Xiaolu.core/Utility/XiaoluConstants.cs
+++ Xiaolu.core/Utility/XiaoluConstants.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace Xiaolu.core.Utility
 {
-    class XiaoluConstants
+    public static class XiaoluConstants
     {
+        public static const string SPLIT_SYMBOL = ",";
     }
 }
