1e9b075427c769c901fc536564ace42344cdf58b
neilchennan <chen_neil@126.com>
2015-11-09 18:58:28 +0800

    add api test main framework(bootstrap)

--- Xiaolu.Webapi/Controllers/UsersController.cs
+++ Xiaolu.Webapi/Controllers/UsersController.cs
@@ -4,36 +4,89 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using Xiaolu.Models.DAL;
+using Xiaolu.Models.Query;
+using Xiaolu.Models.Service;
+using Xiaolu.Utility.Common;
+using Xiaolu.Utility.Helper;
+using Xiaolu.Utility.Result;
 
 namespace Xiaolu.Webapi.Controllers
 {
     public class UsersController : ApiController
     {
         // GET api/users
-        public IEnumerable<string> Get()
+        public Object Get(UserQuery query)
         {
-            return new string[] { "value1", "value2" };
+            string msg;
+            try
+            {
+                if (query == null)
+                    query = new UserQuery();
+                List<User> dbList = BusinessService.GetUserListByQuery(query);
+                //List<UserAPIModel> vmList = UserAPIModel.FromUserList(dbList);
+
+                msg = string.Format(XiaoluResources.MSG_SINGLE_ACTION_SUCCESS, "获取", "用户列表");
+                return new { IsSuccess = true, Message = msg, Obj = dbList };
+            }
+            catch (Exception e)
+            {
+                msg = string.Format(XiaoluResources.MSG_SINGLE_ACTION_FAIL, "获取", "用户列表") + string.Format(XiaoluResources.STR_FAIL_RESAON, ExceptionHelper.GetInnerExceptionInfo(e));
+                return new { IsSuccess = false, Message = msg };
+            }
         }
 
         // GET api/users/5
-        public string Get(int id)
+        public Object Get(string id)
         {
-            return "value";
+            string msg;
+            User objinDb = BusinessService.GetUserById(id);
+            if (objinDb == null)
+            {
+                msg = string.Format(XiaoluResources.MSG_OBJECT_NOT_FOUND_WITH_ID, id);
+                return new { IsSuccess = false, Message = msg };
+            }
+            msg = string.Format(XiaoluResources.MSG_SINGLE_ACTION_SUCCESS, "获取", "用户");
+            return new { IsSuccess = true, Message = msg, Obj = objinDb };
         }
 
         // POST api/users
-        public void Post([FromBody]string value)
+        //新建
+        public Object Post(User obj)
         {
+            BaseActionResult result = BusinessService.CreateUser(obj);
+            return new { IsSuccess = result.IsSuccess, Message = result.Message };
         }
 
         // PUT api/users/5
-        public void Put(int id, [FromBody]string value)
+        //修改
+        public Object Put(string id, User obj)
         {
+            string msg;
+            User objinDb = BusinessService.GetUserById(id);
+            if (objinDb == null)
+            {
+                msg = string.Format(XiaoluResources.MSG_OBJECT_NOT_FOUND_WITH_ID, id);
+                return new { IsSuccess = false, Message = msg };
+            }
+            objinDb.Name = obj.Name;
+            objinDb.Description = obj.Description;
+            objinDb.Gender = obj.Gender;
+            objinDb.Birthday = obj.Birthday;
+            objinDb.Level = obj.Level;
+            objinDb.Status = obj.Status;
+            objinDb.Mobile = obj.Mobile;
+            objinDb.WeixinId = obj.WeixinId;
+
+            BaseActionResult result = BusinessService.UpdateUser(objinDb);
+            return new { IsSuccess = result.IsSuccess, Message = result.Message };
         }
 
         // DELETE api/users/5
-        public void Delete(int id)
+        public Object Delete(string id)
         {
+            BaseActionResult result = BusinessService.BulkDeleteUserByIds(id);
+            return new { IsSuccess = result.IsSuccess, Message = result.Message };
         }
     }
 }
