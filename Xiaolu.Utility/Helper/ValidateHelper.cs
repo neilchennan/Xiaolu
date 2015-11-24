3f79f6ed6c4876bd36eaaaf48fe7f1fc090c8456
zchhaenngg <1076968967@qq.com>
2015-11-24 23:48:25 +0800

    valid email

--- Xiaolu.Utility/Helper/ValidateHelper.cs
+++ Xiaolu.Utility/Helper/ValidateHelper.cs
@@ -11,6 +11,7 @@ namespace Xiaolu.Utility.Helper
     {
         public const int Len5000 = 5000;
         public const string Pattern4Number = @"^[0-9]+$";
+        public const string Pattern4Email = @"^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$";
 
         #region string-验证
 
@@ -43,7 +44,7 @@ namespace Xiaolu.Utility.Helper
 
         public static bool IsValidEmail(string s)
         {
-            return Regex.IsMatch(s, @"^([w-.]+)@(([[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.)|(([w-]+.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(]?)$"); 
+            return Regex.IsMatch(s, Pattern4Email); 
         }
 
         #endregion 
