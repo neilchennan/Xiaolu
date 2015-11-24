d79e746c235ae0ee6317f8d9571de4f075793e46
zchhaenngg <1076968967@qq.com>
2015-11-24 21:39:48 +0800

    test

---
+++ Xiaolu.Utility/Helper/ValidateHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xiaolu.Utility.Helper
+{
+    public class ValidateHelper
+    {
+        #region string-验证
+        #endregion 
+
+        #region datetime-验证
+        #endregion
+    }
+}
